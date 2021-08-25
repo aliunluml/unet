fff269a80375f6456967e88c22373856e3b6b86b
Ali Unlu <33809587+aliunluml@users.noreply.github.com>
2021-08-26 01:36:02 +0300

    Train & Test done. Sample input and output saving TODO

--- main.py
+++ main.py
@@ -10,35 +10,76 @@ from models import UNet
 from lib import DiceLoss,get_dataset,preprocess
 
 t.backends.cudnn.benchmark=True
-BATCH_SIZE=5
+
+if t.cuda.is_available():
+    DEVICE = t.device("cuda")
+else:
+    DEVICE = t.device("cpu")
+
+BATCH_SIZE=2
 NUM_WORKERS=0
 EPOCHS=20
+LR=1e-3
+MOMENTUM=0.9
 
-# def train(net):
-#     loss=0.
-#     for
+def main():
+    raw_dir=Path('raw')
+    train_dataset=get_dataset(raw_dir/'train')
+    test_dataset=get_dataset(raw_dir/'test')
 
+    train_loader=t.utils.data.DataLoader(train_dataset,shuffle=True,batch_size=BATCH_SIZE,pin_memory=False,num_workers=NUM_WORKERS)
+    test_loader=t.utils.data.DataLoader(test_dataset,shuffle=False,batch_size=BATCH_SIZE,pin_memory=False,num_workers=NUM_WORKERS)
 
-raw_dir=Path('raw')
-train_dataset=get_dataset(raw_dir/'train')
-test_dataset=get_dataset(raw_dir/'test')
+    diceloss=DiceLoss()
+    net=UNet().to(DEVICE)
+    opt=t.optim.SGD(net.parameters(),lr=LR,momentum=MOMENTUM)
 
-train_loader=t.utils.data.DataLoader(train_dataset,shuffle=True,batch_size=BATCH_SIZE,pin_memory=False,num_workers=NUM_WORKERS)
-test_loader=t.utils.data.DataLoader(test_dataset,shuffle=False,batch_size=BATCH_SIZE,pin_memory=False,num_workers=NUM_WORKERS)
+    def train(model):
+        model.train()
+        losses=[]
+        for subject_batch in train_loader:
+            img_batch,seg_batch=preprocess(subject_batch)
+            img_batch.to(DEVICE)
+            seg_batch.to(DEVICE)
 
-diceloss=DiceLoss()
+            logits=model(img_batch)
 
-# def train(model):
-#     for subject_batch in train_loader:
-#         img_batch,seg_batch=preprocess(subject_batch)
-#         logits=model(img_batch)
-#         loss=diceloss(logits,labels)
-#         loss.backward()
-#         break
+            loss=diceloss(logits,labels)
+            losses.append(loss.item())
+            loss.backward()
 
+            opt.step()
+            opt.zero_grad()
 
+        return losses.mean()
 
-# model=UNet()
-# opt=
-#
-# for epoch in range(0,EPOCHS):
+    def test(model):
+        model.eval()
+        losses=[]
+        with t.no_grad():
+            for subject_batch in test_loader:
+                img_batch,seg_batch=preprocess(subject_batch)
+                img_batch.to(DEVICE)
+                seg_batch.to(DEVICE)
+
+                logits=model(img_batch)
+                loss=diceloss(logits,labels)
+                losses.append(loss.item())
+
+        return losses.mean()
+
+    dict={'epoch':[],'train_losses':[],'test_losses':[]}
+    for epoch in range(0,EPOCHS):
+        train_loss=train(net)
+        test_loss=test(net)
+        dict['train_losses'].append(train_loss)
+        dict['test_losses'].append(test_loss)
+        dict['epoch'].append(epoch)
+
+    df=pd.DataFrame(dict)
+    df.to_csv('out.csv')
+
+    t.save(net.state_dict(),'weights.pt')
+
+if __name__ == "__main__":
+    main()
