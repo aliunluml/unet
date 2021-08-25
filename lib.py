5c1a3899f1ab46fdcfbaf57ef7141218a634da07
Ali Unlu <33809587+aliunluml@users.noreply.github.com>
2021-08-25 19:57:25 +0300

    Flattening subject causes non-IID samples

--- lib.py
+++ lib.py
@@ -1,4 +1,68 @@
 import torch as t
+import torchio as tio
+import torchvision as tv
+
+
+
+def get_transforms(landmarks):
+    ras_transform=tio.ToCanonical()
+    crop_transform=tio.CropOrPad((512,512,128))
+    hist_transform = tio.HistogramStandardization(landmarks)
+    znorm_transform = tio.ZNormalization(masking_method=tio.ZNormalization.mean)
+
+    transforms=tio.Compose([ras_transform,crop_transform,hist_transform,znorm_transform])
+    return transforms
+
+def get_dataset(dataset_dir):
+    seg_paths = sorted(dataset_dir.glob('*liver.nii.gz'))
+    img_paths = sorted(dataset_dir.glob('*orig.nii.gz'))
+
+    subjects=[]
+    for seg_path,img_path in zip(seg_paths,img_paths):
+        subject = tio.Subject(mri=tio.ScalarImage(img_path),liver=tio.LabelMap(seg_path))
+        subjects.append(subject)
+
+    # Preprocessing
+    hist_landmarks = tio.HistogramStandardization.train(img_paths,output_path=dataset_dir/'landmarks.npy')
+    dict={'mri': hist_landmarks}
+    transforms=get_transforms(dict)
+    dataset = tio.SubjectsDataset(subjects,transform=transforms)
+    return dataset
+
+
+# class LiverDataset(t.utils.data.Dataset):
+#     def __init__(self,path,undersample=True,factor=2):
+#         super(LiverDataset,self).__init__()
+#         self.sdataset=get_dataset(path)
+#         self.factor=factor if undersample else 1
+#
+#     def __len__(self):
+#         return len(self.sdataset)
+#
+#     def slice(self,x):
+#         print(x.shape)
+#         x=t.movedim(x,-1,0)
+#         print(x.shape)
+#         x=t.flatten(x,start_dim=0,end_dim=1)
+#         print(x.shape)
+#         return x
+#
+#     def __getitem__(self,i):
+#         sbatch=self.sdataset.__getitem__(i)
+#         imgs=self.slice(sbatch['mri']['data'])
+#         segs=self.slice(sbatch['liver']['data'])
+#
+#         imgs=imgs[:,::self.factor,::self.factor]
+#         segs=segs[:,::self.factor,::self.factor]
+#
+#         return imgs,segs
+#
+# def slice()
+
+# def preprocess(subjects):
+#     imgs=self.slice(subjects['mri']['data'])
+#     segs=self.slice(subjects['liver']['data'])
+
 
 
 class DiceLoss(t.nn.Module):
--- main.py
+++ main.py
@@ -7,37 +7,12 @@ import matplotlib.pyplot as plt
 import pytorch_lightning as pl
 
 from models import UNet
-from lib import DiceLoss
+from lib import DiceLoss,LiverDataset
 
 t.backends.cudnn.benchmark=True
 BATCH_SIZE=5
 NUM_WORKERS=0
 EPOCHS=20
-
-def get_transforms(landmarks):
-    ras_transform=tio.ToCanonical()
-    crop_transform=tio.CropOrPad((512,512,128))
-    hist_transform = tio.HistogramStandardization(landmarks)
-    znorm_transform = tio.ZNormalization(masking_method=tio.ZNormalization.mean)
-
-    transforms=tio.Compose([ras_transform,crop_transform,hist_transform,znorm_transform])
-    return transforms
-
-def get_dataset(dataset_dir):
-    seg_paths = sorted(dataset_dir.glob('*liver.nii.gz'))
-    img_paths = sorted(dataset_dir.glob('*orig.nii.gz'))
-
-    subjects=[]
-    for seg_path,img_path in zip(seg_paths,img_paths):
-        subject = tio.Subject(mri=tio.ScalarImage(img_path),liver=tio.LabelMap(seg_path))
-        subjects.append(subject)
-
-    # Preprocessing
-    hist_landmarks = tio.HistogramStandardization.train(img_paths,output_path=dataset_dir/'landmarks.npy')
-    dict={'mri': hist_landmarks}
-    transforms=get_transforms(dict)
-    dataset = tio.SubjectsDataset(subjects,transform=transforms)
-    return dataset
 
 # def train(net):
 #     loss=0.
@@ -45,15 +20,15 @@ def get_dataset(dataset_dir):
 
 
 raw_dir=Path('raw')
-train_dataset=get_dataset(raw_dir/'train')
-test_dataset=get_dataset(raw_dir/'test')
+train_dataset=LiverDataset(raw_dir/'train')
+test_dataset=LiverDataset(raw_dir/'test')
 
 train_loader=t.utils.data.DataLoader(train_dataset,shuffle=True,batch_size=BATCH_SIZE,pin_memory=False,num_workers=NUM_WORKERS)
 test_loader=t.utils.data.DataLoader(test_dataset,shuffle=False,batch_size=BATCH_SIZE,pin_memory=False,num_workers=NUM_WORKERS)
 
-for subjects in train_loader:
-    print(subjects['mri']['data'].shape)
-    print(subjects['liver']['data'].shape)
+for batch,labels in train_loader:
+    print(batch.shape)
+    print(labels.shape)
     break
 
 
